c4fcabbeab22af3d702353fa3342182d0da0a629
Vandegir <134431784+kristinazudiaeva@users.noreply.github.com>
2024-01-11 00:25:00 +0300

    Create Contact.java

---
+++ lb3/Contact.java
@@ -0,0 +1,13 @@
+import javax.xml.namespace.QName;
+
+public class Contact {
+    String name;
+    String email;
+    String contactInfo;
+
+    Contact(String name, String email, String contactInfo) {
+      this.name = name;
+      this.email = email;
+      this.contactInfo = contactInfo;
+    }
+}
